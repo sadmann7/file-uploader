e624c49bbd158b1a7b7e758331b68de8536984b2
sadmann7 <sadmann898@gmail.com>
2024-04-14 05:06:08 +0600

    feat: update github issues

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
